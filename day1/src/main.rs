874778886d45262fb7e83377d1cd1b6171a519b5
Doug Stephen <dstephen@ihmc.us>
2019-10-15 10:53:42 -0500

    Day 1, Part 2 solved.

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::fs;
 use std::num::ParseIntError;
 
@@ -5,18 +6,53 @@ type Result<T> = std::result::Result<T, Box<dyn ::std::error::Error>>;
 type ParseResult = std::result::Result<Vec<i32>, ParseIntError>;
 
 fn main() -> Result<()> {
-    process_input("day1_input.txt")
+    process_input_part_1("day1_input.txt")?;
+    process_input_part_2("day1_input.txt")?;
+
+    Ok(())
 }
 
-fn process_input(filename: &str) -> Result<()> {
-    match fs::read_to_string(filename)?
+fn process_input_part_1(filename: &str) -> Result<()> {
+    let contents = fs::read_to_string(filename)?;
+
+    match parse_integers_from_contents(contents) {
+        Ok(parsed_ints) => {
+            println!("Final frequency: {:?}", parsed_ints.iter().sum::<i32>());
+            Ok(())
+        }
+        Err(e) => Err(From::from(format!("Could not parse input file: {}", e))),
+    }
+}
+
+fn parse_integers_from_contents(contents: String) -> ParseResult {
+    contents
         .lines()
         .map(|line| line.parse::<i32>())
         .collect::<ParseResult>()
-    {
-        Ok(val) => {
-            println!("Final frequency: {:?}", val.iter().sum::<i32>());
-            Ok(())
+}
+
+fn process_input_part_2(filename: &str) -> Result<()> {
+    let contents = fs::read_to_string(filename)?;
+
+    match parse_integers_from_contents(contents) {
+        Ok(parsed_ints) => {
+            let mut current_freq = 0;
+            let mut frequencies = HashSet::new();
+            frequencies.insert(current_freq);
+
+            loop {
+                for freq in parsed_ints.iter() {
+                    current_freq += *freq;
+
+                    if frequencies.contains(&current_freq) {
+                        println!("First doubled frequency: {:?}", current_freq);
+                        return Ok(())
+                    }
+                    else {
+                        frequencies.insert(current_freq);
+                    }
+                }
+            }
         }
         Err(e) => Err(From::from(format!("Could not parse input file: {}", e))),
     }
